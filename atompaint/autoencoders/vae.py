fix: correct the KL divergence calculation

atompaint/autoencoders/vae.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 from functools import cached_property
 
 from atompaint.type_hints import OptFactory
+from macromol_gym_unsupervised import MakeSampleArgs
 
 class VariationalAutoencoder(L.LightningModule):
     """
@@ -113,19 +114,29 @@ class VaeLoss:
     def total(self):
         return self.data + self.beta * self.prior
 
-def make_vae_image_tensors(db, db_cache, rng, zone_id, *, img_params):
+def make_vae_image_tensors(sample: MakeSampleArgs, *, img_params):
     from macromol_gym_unsupervised import make_unsupervised_image_sample
 
-    x = make_unsupervised_image_sample(
-            db, db_cache, rng, zone_id,
-            img_params=img_params,
-    )
+    x = make_unsupervised_image_sample(sample, img_params=img_params)
+
     return dict(
             rng=x['rng'],
             image=x['image'],
     )
 
-def kl_divergence_vs_std_normal(mean, std):
+def kl_divergence_vs_std_normal(mean, std, dim=[1,2,3,4]):
+    """
+    Calculate the KL-divergence between the given diagonal Gaussian 
+    distribution and a Gaussian distribution with a mean of 0 and a standard 
+    deviation of 1.
+
+    The *dim* argument specifies over which dimensions the KL-divergence is 
+    summed.  It is averaged over all other dimensions.  The default is 
+    appropriate for 3D images with a batch dimension.
+    """
     # https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence#Multivariate_normal_distributions
-    return 0.5 * torch.sum(std + mean**2 - 1 - torch.log(std))
+    var = std**2
+    return torch.mean(
+            0.5 * torch.sum(var + mean**2 - 1 - torch.log(var), dim=dim)
+    )
 

tests/test_autoencoders_vae.py
@@ -7,6 +7,7 @@ from hypothesis.strategies import composite, floats, one_of, just
 from hypothesis.extra.numpy import arrays
 from torch.distributions.multivariate_normal import MultivariateNormal
 from torch.distributions.kl import kl_divergence
+from functools import partial
 
 @composite
 def mean_std_tensors(draw):
@@ -30,7 +31,7 @@ def mean_std_tensors(draw):
     return torch.from_numpy(mean), torch.from_numpy(std)
 
 def test_kl_divergence():
-    d = _ap.kl_divergence_vs_std_normal
+    d = partial(_ap.kl_divergence_vs_std_normal, dim=[0])
 
     mean = torch.zeros(2)
     std = torch.ones(2)
@@ -44,10 +45,12 @@ def test_kl_divergence_torch(mean_std):
     mean, std = mean_std
     n = mean.nelement()
 
-    N0 = MultivariateNormal(torch.zeros(n), torch.eye(n))
-    N1 = MultivariateNormal(mean.flatten(), torch.diag(std.flatten()))
+    assume(not (std**2).isinf().any())
 
-    kl_ap = _ap.kl_divergence_vs_std_normal(mean, std)
+    N0 = MultivariateNormal(torch.zeros(n), torch.eye(n))
+    N1 = MultivariateNormal(mean.flatten(), torch.diag(std.flatten()**2))
+
+    kl_ap = _ap.kl_divergence_vs_std_normal(mean, std, dim=list(range(mean.ndim)))
 
     # Note that `N1` and `N0` are swapped relative to what you might expect; 
     # the standard normal is not the reference distribution.  This is how 
@@ -58,6 +61,6 @@ def test_kl_divergence_torch(mean_std):
     # infinite and the other is just very large.  This isn't a problem, because 
     # we're not trying to exactly reproduce the torch implementation, but it 
     # will cause the following assertion to fail.  So we skip these cases.
-    assume(kl_ap.isfinite().all() == kl_torch.isfinite().all())
+    assume(kl_ap.isfinite() and kl_torch.isfinite())
 
     torch.testing.assert_close(kl_ap, kl_torch)